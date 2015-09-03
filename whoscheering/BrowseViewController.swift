1e2ab7e849703eddc8fde0101465936eb0f3532f
sponrad <sponrad@gmail.com>
2015-09-02 21:33:25 -0700

    better resume handling, crap and search in first browse

--- whoscheering/BrowseViewController.swift
+++ whoscheering/BrowseViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class BrowseViewController: UITableViewController {
+class BrowseViewController: UITableViewController, UISearchResultsUpdating {
     
     @IBOutlet var browseTable: UITableView!
     
+    let searchController = UISearchController(searchResultsController: nil)
     var categories: [String] = []
+    var filteredCategories: [String] = []
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchController.searchResultsUpdater = self
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchBar.sizeToFit()
+        self.tableView.tableHeaderView = searchController.searchBar
         
         let documentsFolder = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
         let path = documentsFolder.stringByAppendingPathComponent("ff.db")
@@ -42,6 +51,18 @@ class BrowseViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let sb = searchController.searchBar
+        let target = sb.text
+        self.filteredCategories = self.categories.filter {
+            s in
+            let options = NSStringCompareOptions.CaseInsensitiveSearch
+            let found = s.rangeOfString(target, options: options)
+            return (found != nil)
+        }
+        self.tableView.reloadData()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -59,59 +80,32 @@ class BrowseViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return categories.count
+        if searchController.active{
+            return filteredCategories.count
+        }
+        else {
+            return categories.count
+        }
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("categoryCell", forIndexPath: indexPath) as! UITableViewCell
         
-        // Configure the cell
-        cell.textLabel!.text = self.categories[indexPath.row]
-        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        if searchController.active {
+            cell.textLabel!.text = self.filteredCategories[indexPath.row]
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        }
+        else{
+            // Configure the cell
+            cell.textLabel!.text = self.categories[indexPath.row]
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        }
+
 
         return cell
     }
    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
